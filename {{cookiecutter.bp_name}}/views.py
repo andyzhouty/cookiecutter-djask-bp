345fdfdd3d7d76cb96d5d2d13c09a040ac4484df
andyzhou <andyforever0108@outlook.com>
2022-07-22 17:40:11 +0800

    feat: Remove the trailing  in the built-in route

--- {{cookiecutter.bp_name}}/views.py
+++ {{cookiecutter.bp_name}}/views.py
@@ -8,6 +8,6 @@ from djask import {{ class_name }}
 
 {{ '{0}_bp = {1}("{0}", __name__, url_prefix="/{0}")'.format(bp_name, class_name) }}
 
-@{{ bp_name }}_bp.route("/")
+@{{ bp_name }}_bp.route("")
 def bp_index():
     return "Hello World!"
